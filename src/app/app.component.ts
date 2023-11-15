db437de11ada941a6885cad38d636b93435cc6bd
martvaha <martvaha@gmail.com>
2023-11-15 08:48:05 +0200

    remove unused imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,6 @@ import { MatButtonModule } from "@angular/material/button";
 import { MatIconModule } from "@angular/material/icon";
 import { MatInputModule } from "@angular/material/input";
 import { MatFormFieldModule } from "@angular/material/form-field";
-import { Observable } from "rxjs";
-import { Message } from "./openai.schema";
 import { FormsModule } from "@angular/forms";
 
 @Component({
